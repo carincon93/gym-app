add4a0e129615d32d23323d7df56818476e0ba70
Cristian Camilo Vasquez Rincón <carincon93@gmail.com>
2025-05-07 09:39:41 -0500

    feat: add TypeScript definitions for Astro and PWA plugins; implement service worker registration

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,14 +4,42 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 
 import tailwindcss from "@tailwindcss/vite";
+import AstroPWA from "@vite-pwa/astro";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://carincon93.github.io",
   base: "gym-app",
-  integrations: [react()],
+  integrations: [
+    react(),
+    AstroPWA({
+      mode: "development",
+      base: "/gym-app/",
+      scope: "/gym-app/",
+      includeAssets: ["favicon.svg"],
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Astro PWA",
+        short_name: "Astro PWA",
+        theme_color: "#ffffff",
+        icons: [],
+      },
+      workbox: {
+        navigateFallback: "/gym-app/index.html",
+        globPatterns: ["**/*.{css,js,html,svg,png,ico,txt}"],
+      },
+      devOptions: {
+        enabled: true,
+        navigateFallbackAllowlist: [/^\/$/],
+      },
+      experimental: {
+        directoryAndTrailingSlashHandler: true,
+      },
+    }),
+  ],
 
   vite: {
+    // @ts-ignore
     plugins: [tailwindcss()],
   },
 });
