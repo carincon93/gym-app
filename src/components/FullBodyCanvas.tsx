d8cd10d2ae067bfbc47ff0d1cfcf7884c334f703
Cristian Camilo Vasquez Rincón <carincon93@gmail.com>
2025-05-09 17:39:25 -0500

    feat: add alert dialog to display sets per muscle group in FullBodyCanvas

--- src/components/FullBodyCanvas.tsx
+++ src/components/FullBodyCanvas.tsx
@@ -5,7 +5,17 @@ import type { Record, Week } from "@/lib/types";
 import { getRecords } from "@/services/records.service";
 import { toast } from "sonner";
 import { Button } from "./ui/button";
-import { Play } from "lucide-react";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from "@/components/ui/alert-dialog";
+import { Info, Play } from "lucide-react";
 
 type BodyCanvasProps = {
   canvasId: string;
@@ -15,6 +25,7 @@ export default function FullBodyCanvas({ canvasId }: BodyCanvasProps) {
   const [week, setWeek] = useState<Week>();
   const [records, setRecords] = useState<Record[]>([]);
   const riveRef = useRef<rive.Rive | null>(null);
+  const [opeDialog, setOpenDialog] = useState<boolean>(false);
 
   const COLORS = {
     excess: { r: 255, g: 110, b: 96 },
@@ -22,6 +33,29 @@ export default function FullBodyCanvas({ canvasId }: BodyCanvasProps) {
     minimum: { r: 211, g: 172, b: 235 },
     inactive: { r: 159, g: 170, b: 204 },
   };
+
+  const mrv = (machineIds: string[]) =>
+    records.filter((record) =>
+      machineIds.includes(record.machineId.toString())
+    );
+
+  const chest = mrv(["1", "2", "3", "4", "5", "6"]);
+  const quads = mrv(["7", "8", "9", "10"]);
+  const abs = mrv(["11", "12"]);
+  const obliques = mrv(["13"]);
+  const biceps = mrv(["14", "15", "16", "17", "18", "53"]);
+  const shoulders = mrv(["19", "20", "21", "22"]);
+  const forearms = mrv(["23", "24"]);
+  const adductors = mrv(["25"]);
+  const calves = mrv(["26", "27", "28"]);
+  const traps = mrv(["29", "30"]);
+  const neck = mrv(["31", "32"]);
+  const hips = mrv(["33", "34", "35", "36"]);
+  const hamstrings = mrv(["37", "38"]);
+  const glutes = mrv(["39", "40", "41"]);
+  const lats = mrv(["42", "43", "44", "45", "46", "47", "54"]);
+  const triceps = mrv(["48", "49", "50", "51"]);
+  const lowerBack = mrv(["52"]);
 
   // Función para crear instancias de animaciones Rive
   const createRiveInstance = () => {
@@ -42,39 +76,16 @@ export default function FullBodyCanvas({ canvasId }: BodyCanvasProps) {
         src: "/gym-app/animations/full-body.riv",
         autoBind: true,
         onLoad: () => {
-          const mrv = (machineIds: string[]) =>
-            records.filter((record) =>
-              machineIds.includes(record.machineId.toString())
-            );
-
-          const chest = mrv(["1", "2", "3", "4", "5", "6"]);
-          const quads = mrv(["7", "8", "9", "10"]);
-          const abs = mrv(["11", "12"]);
-          const obliques = mrv(["13"]);
-          const biceps = mrv(["14", "15", "16", "17", "18", "53"]);
-          const shoulders = mrv(["19", "20", "21", "22"]);
-          const forearms = mrv(["23", "24"]);
-          const adductors = mrv(["25"]);
-          const calves = mrv(["26", "27", "28"]);
-          const traps = mrv(["29", "30"]);
-          const neck = mrv(["31", "32"]);
-          const hips = mrv(["33", "34", "35", "36"]);
-          const hamstrings = mrv(["37", "38"]);
-          const glutes = mrv(["39", "40", "41"]);
-          const lats = mrv(["42", "43", "44", "45", "46", "47", "54"]);
-          const triceps = mrv(["48", "49", "50", "51"]);
-          const lowerBack = mrv(["52"]);
-
-            const checkOptimalSetsByWeek = (qty: number) => ({
+          const checkOptimalSetsByWeek = (qty: number) => ({
             status:
               qty === 0
-              ? COLORS.inactive
-              : qty < 12
-              ? COLORS.minimum
-              : qty <= 20
-              ? COLORS.optimum
-              : COLORS.excess,
-            });
+                ? COLORS.inactive
+                : qty < 12
+                ? COLORS.minimum
+                : qty <= 20
+                ? COLORS.optimum
+                : COLORS.excess,
+          });
 
           // The Rive object is now loaded and ready to use.
           const vmi = instance.viewModelInstance;
@@ -153,7 +164,7 @@ export default function FullBodyCanvas({ canvasId }: BodyCanvasProps) {
   return (
     <div className="relative" id="full">
       <div className="flex flex-col justify-center items-center">
-        <canvas className="mask-fade" id={canvasId} width="390" height="844" />
+        <canvas id={canvasId} width="390" height="844" />
 
         <div className="fixed bottom-72 left-0 right-0 text-center text-slate-500 mt-2 text-xs px-4 md:w-5/12 mx-auto">
           <div className="bg-white p-2 rounded-md shadow-md block">
@@ -189,6 +200,9 @@ export default function FullBodyCanvas({ canvasId }: BodyCanvasProps) {
                 />
                 <span>{">"} 20 sets per muscle group</span>
               </div>
+              <Button onClick={() => setOpenDialog(true)} variant="outline">
+                <Info />
+              </Button>
             </div>
           </div>
         </div>
@@ -219,6 +233,39 @@ export default function FullBodyCanvas({ canvasId }: BodyCanvasProps) {
           </Button>
         </div>
       </div>
+
+      <AlertDialog open={opeDialog} onOpenChange={setOpenDialog}>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Sets per muscle group</AlertDialogTitle>
+            <AlertDialogDescription></AlertDialogDescription>
+            <div className="text-xs">
+              <ul>
+                <li>{chest.length} sets for chest</li>
+                <li>{quads.length} sets for quads</li>
+                <li>{abs.length} sets for abs</li>
+                <li>{obliques.length} sets for Obliques</li>
+                <li>{biceps.length} sets for biceps</li>
+                <li>{shoulders.length} sets for shoulders</li>
+                <li>{forearms.length} sets for forearms</li>
+                <li>{adductors.length} sets for adductors</li>
+                <li>{calves.length} sets for calves</li>
+                <li>{traps.length} sets for traps</li>
+                <li>{neck.length} sets for neck</li>
+                <li>{hips.length} sets for hips</li>
+                <li>{hamstrings.length} sets for hamstrings</li>
+                <li>{glutes.length} sets for glutes</li>
+                <li>{lats.length} sets for lats</li>
+                <li>{triceps.length} sets for tricpes</li>
+                <li>{lowerBack.length} sets for lower back</li>
+              </ul>
+            </div>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>Close</AlertDialogCancel>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
     </div>
   );
 }
